2a4047b621dcf7a47a469ce22b7e429fe0cca783
fizik003 <kassik0boks@gmail.com>
2020-11-04 16:40:13 +0300

    refactor: delete console.log

--- src/common/hashHelper.js
+++ src/common/hashHelper.js
@@ -2,9 +2,7 @@ const bcrypt = require('bcrypt');
 
 const generateHashPassword = async password => {
   const salt = await bcrypt.genSalt(10);
-  console.log(salt);
   const hash = await bcrypt.hash(password, salt);
-  console.log(hash);
   return hash;
 };
 
